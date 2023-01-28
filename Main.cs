34417120faf645007e855e025dae965614e1c2a6
Shinter <25801961+DjShinter@users.noreply.github.com>
2023-01-29 02:41:43 +0400

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,37 @@
+﻿using ABI_RC.Core.Networking;
+using HarmonyLib;
+using MelonLoader;
+
+namespace FixReconnectKey
+{
+    public static class ModBuildInfo
+    {
+        public const string Name = "FixReconnectKey";
+        public const string Author = "Shin";
+        public const string Version = "1.0";
+        public const string Description = "This removes the minus hotkey since it was never supposed to be shipped.";
+        public const string DownloadLink = "https://github.com/DjShinter";
+    }
+
+    public class FixReconnectKeyMod : MelonMod
+    {
+        public static HarmonyLib.Harmony Instance = new HarmonyLib.Harmony("FixReconnectKeyMod");
+        public override void OnInitializeMelon()
+        {
+            Instance.PatchAll();
+            MelonLogger.Msg("NetworkManager Patched");
+            MelonLogger.Msg("This should only be used in 2022r170, otherwise this may cause issues!");
+        }
+
+        [HarmonyPatch(typeof(NetworkManager), "Update")]
+        public class RemoveNetworkManagerKeybinding
+        {
+            [HarmonyPrefix]
+            public static bool Prefix()
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Reflection;
+
+using System.Runtime.InteropServices;
+using MelonLoader;
+using FixReconnectKey;
+
+[assembly: AssemblyTitle(ModBuildInfo.Name)]
+[assembly: AssemblyDescription(ModBuildInfo.Description)]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct(ModBuildInfo.Name)]
+[assembly: AssemblyCopyright("Copyright © 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(ModBuildInfo.Version)]
+[assembly: AssemblyFileVersion(ModBuildInfo.Version)]
+[assembly: MelonInfo(typeof(FixReconnectKeyMod), ModBuildInfo.Name, ModBuildInfo.Version, ModBuildInfo.Author, ModBuildInfo.DownloadLink)]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+[assembly: MelonColor(ConsoleColor.DarkMagenta)]
+[assembly: MelonPriority(0)]
